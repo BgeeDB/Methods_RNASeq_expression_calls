c0f9c43ac49d31603dcd50a938c2cdea449d0ecb
SFonsecaCosta <sara.fonsecacosta@unil.ch>
2020-06-26 07:56:18 +0200

    highlight  blood in the plots

--- R_script_generateFigures/generate_figures.R
+++ R_script_generateFigures/generate_figures.R
@@ -1,4 +1,4 @@
-## Code to generate the figures to the Bgee Call paper
+## Generate the figures for GTeX data (over tissues) and different cutoff (over species)
 ##  libraries used
 library(ggplot2)
 
@@ -6,15 +6,18 @@ pathToFIles <- "Files_to_generate_figures/"
 outFiles <- "Figure_BgeeCallpaper/"
 
 Threshold_2 <- read.table(paste0(pathToFIles, "/", "presence_absence_all_samples_TPM-treshold.txt"), header=TRUE, sep="\t", comment.char="")
+colnames(Threshold_2)[1] <- "libraryId" 
 bgee_withoutDecov <- read.table(paste0(pathToFIles, "/", "presence_absence_all_samples_without_decov.txt"), header=TRUE, sep="\t", comment.char="")
+colnames(bgee_withoutDecov)[1] <- "libraryId" 
 bgeeThreshold <- read.table(paste0(pathToFIles, "/", "presence_absence_all_samples_14.txt"), header=TRUE, sep="\t", comment.char="")
+colnames(bgeeThreshold)[1] <- "libraryId"
 bgeeThreshold_without_N <- read.table(paste0(pathToFIles, "/", "presence_absence_all_samples_without_N.txt"), header=TRUE, sep="\t", comment.char="")
-
-
+colnames(bgeeThreshold_without_N)[1] <- "libraryId"
 infoFile <- read.table(paste0(pathToFIles, "/", "info_rnaSeq.tsv"), header=TRUE, sep="\t", comment.char="")
+colnames(infoFile)[1] <- "libraryId"
 
 ## Threshold = 2
-gg_Threshold_2 <- ggplot(data = Threshold_2, mapping = aes(x = Threshold_2$organism, y = Threshold_2$proportionCodingPresent, label=Threshold_2$organism)) +
+gg_Threshold_2 <- ggplot(data = Threshold_2, mapping = aes(x = organism, y = proportionCodingPresent, label=organism)) +
   geom_boxplot(alpha = 1) +
   labs(x = "", y = "% Protein Coding Genes") +
   scale_y_continuous(limits=c(0,100)) +
@@ -25,7 +28,7 @@ gg_Threshold_2
 dev.off()
 
 ## Bgee without deconvolution
-gg_bgeeWithoutDec <- ggplot(data = bgee_withoutDecov, mapping = aes(x = bgee_withoutDecov$organism, y = bgee_withoutDecov$proportionCodingPresent, label=bgee_withoutDecov$organism)) +
+gg_bgeeWithoutDec <- ggplot(data = bgee_withoutDecov, mapping = aes(x = organism, y = proportionCodingPresent, label=organism)) +
   geom_boxplot(alpha = 1) +
   labs(x = "", y = "% Protein Coding Genes") +
   scale_y_continuous(limits=c(0,100)) +
@@ -36,19 +39,18 @@ gg_bgeeWithoutDec
 dev.off()
 
 ## Bgee Intergenic cut-off
-gg_bgee <- ggplot(data = bgeeThreshold, mapping = aes(x = bgeeThreshold$organism, y = bgeeThreshold$proportionCodingPresent, label=bgeeThreshold$organism)) +
+gg_bgee <- ggplot(data = bgeeThreshold, mapping = aes(x = organism, y = proportionCodingPresent, label=organism)) +
   geom_boxplot(alpha = 1) +
   labs(x = "", y = "% Protein Coding Genes") +
   scale_y_continuous(limits=c(0,100)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
 
 pdf(file = paste0(outFiles, "/Bgee_threshold_allSpecies.pdf"), width = 14, height = 7)
 gg_bgee
 dev.off()
 
 ## Bgee Intergenic cut-off without N
-gg_bgee_wo_N <- ggplot(data = bgeeThreshold_without_N, mapping = aes(x = bgeeThreshold_without_N$organism, y = bgeeThreshold_without_N$proportionCodingPresent, label=bgeeThreshold_without_N$organism)) +
+gg_bgee_wo_N <- ggplot(data = bgeeThreshold_without_N, mapping = aes(x = organism, y = proportionCodingPresent, label=organism)) +
   geom_boxplot(alpha = 1) +
   labs(x = "", y = "% Protein Coding Genes") +
   scale_y_continuous(limits=c(0,100)) +
@@ -62,27 +64,35 @@ dev.off()
 gtexData <- dplyr::filter(infoFile, experimentId == "SRP012682")
 
 ## Gtex with Threshold = 2
-gtexThreshold <- merge(Threshold_2, gtexData, by="X.libraryId")
+gtexThreshold <- merge(Threshold_2, gtexData, by="libraryId")
 
-gtexThreshold_cutoff <- ggplot(data = gtexThreshold, mapping = aes(x = gtexThreshold$uberonName, y = gtexThreshold$proportionCodingPresent, label=gtexThreshold$uberonName)) +
+gtexThreshold_cutoff <- gtexThreshold %>%
+  mutate( type=ifelse(uberonName=="blood","Highlighted","Normal")) %>%
+  ggplot(aes(x = uberonName, y = proportionCodingPresent, label=uberonName, fill=type, alpha=type)) +
   geom_boxplot(alpha = 1) +
   labs(x = "", y = "% Protein Coding Genes") +
   scale_y_continuous(limits=c(0,100)) +
-  theme(axis.text.x = element_text(angle = 55, hjust = 1))
+  scale_fill_manual(values=c("red", "white")) +
+  scale_alpha_manual(values=c(1,0.1)) +
+  theme(axis.text.x = element_text(angle = 55, hjust = 1), legend.position = "none")
 
-pdf(file = paste0(outFiles, "/GTEx_threshold.pdf"), width = 14, height = 7)
+pdf(file = paste0(outFiles, "/GTEx_threshold.pdf"), width = 16, height = 7)
 gtexThreshold_cutoff
 dev.off()
 
 
 ## Gtex with Bgee cut-off
-gtexBgee <- merge(bgeeThreshold, gtexData, by="X.libraryId")
+gtexBgee <- merge(bgeeThreshold, gtexData, by="libraryId")
 
-gtexBgee_cutoff <- ggplot(data = gtexBgee, mapping = aes(x = gtexBgee$uberonName, y = gtexBgee$proportionCodingPresent, label=gtexBgee$uberonName)) +
+gtexBgee_cutoff <- gtexBgee %>%
+  mutate( type=ifelse(uberonName=="blood","Highlighted","Normal")) %>%
+  ggplot(aes(x = uberonName, y = proportionCodingPresent, label=uberonName, fill=type, alpha=type)) +
   geom_boxplot(alpha = 1) +
   labs(x = "", y = "% Protein Coding Genes") +
   scale_y_continuous(limits=c(0,100)) +
-  theme(axis.text.x = element_text(angle = 55, hjust = 1))
+  scale_fill_manual(values=c("red", "white")) +
+  scale_alpha_manual(values=c(1,0.1)) +
+  theme(axis.text.x = element_text(angle = 55, hjust = 1), legend.position = "none")
 
 pdf(file = paste0(outFiles, "/GTEx_Bgee_Cutoff.pdf"), width = 16, height = 7)
 gtexBgee_cutoff
